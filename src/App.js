8dfa3f1b32c39a43083d1cd5c7fcb9d923a52585
Diksharayat <rayatdiksha787@gmail.com>
2024-01-09 12:31:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navbar from "./Components/Navbar/Navbar";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Works from "./Components/Works/Works";
+import Service from "./Components/Service/Service";
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+    <Header />
+      <Service />
+      <Works />
+      <Footer />
+    </div>
+
+  );
+}
+
+export default App;
